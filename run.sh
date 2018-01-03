38facc638327b94d34a72f8cebd2d22fde1a9718
Ralf Hense <ralf.hense@1und1.de>
2018-01-03 22:44:27 +0100

    helper to easy start container

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ "$ETCD" == "" ]]; then
+    echo Variable ETCD is undefined.
+    echo "looks like: http://<ip>:2379 for single node"
+    echo "or comma separated for multi hosts
+    exit 1
+fi
+
+docker run --name haproxy zwotzie/easy-lb-haproxy --rm -p 81:80 -e ETCD_PEERS=$ETCD -e HAPROXY_STATS=1
