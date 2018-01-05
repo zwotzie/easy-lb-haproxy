643e8bda85ea9c11597d2a394136219cde6d1087
Ralf Hense <ralf.hense@1und1.de>
2018-01-05 19:32:32 +0100

    if/else for differentiating zookeeper or etcd

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-if [[ "$ETCD" == "" ]]; then
-    echo Variable ETCD is undefined.
-    echo "looks like: http://<ip>:2379 for single node"
-    echo "or comma separated for multi hosts"
-    echo "export ETCD=http://xxx:2379"
-    exit 1
-fi
+if [ ! -z "$ZK" ]; then
+    echo "run.sh: is using zookeeper: ${ZK}"
 
-docker run --name haproxy --rm -p 81:80 -e ETCD_PEERS=$ETCD -e HAPROXY_STATS=1 zwotzie/easy-lb-haproxy
+    docker run -d --name haproxy --rm -p 81:80 -e ZK=${ZK} -e HAPROXY_STATS=1 zwotzie/easy-lb-haproxy
+
+else
+    echo "run.sh: is using ETCD: ${ETCD}"
+
+    if [[ "$ETCD" == "" ]]; then
+        echo Variable ETCD is undefined.
+        echo "looks like: http://<ip>:2379 for single node"
+        echo "or comma separated for multi hosts"
+        echo "export ETCD=http://xxx:2379"
+        exit 1
+    fi
+
+    docker run --name haproxy --rm -p 81:80 -e ETCD_PEERS=${ETCD} -e HAPROXY_STATS=1 zwotzie/easy-lb-haproxy
+
+fi
--- watcher.sh
+++ watcher.sh
@@ -1,27 +1,46 @@
 #!/bin/sh
 
-# make sure the /services key is set in etcd before we start
-for ETCD_URL in ${ETCD_PEERS//,/ }; do
-    curl -s -f $ETCD_URL/v2/keys/services -XPUT -d dir=true
-    if [ "$?" = "0" ]; then
-        break
-    fi
-done
+if [ ! -z "$ZK" ]; then
+    echo "watcher.sh: is using zookeeper: ${ZK}"
 
-# attempt to generate a first configuration
-ETCD_NODES="-node ${ETCD_PEERS//,/ -node }"
-while [ true ]; do
-    ./confd -onetime $ETCD_NODES -config-file /etc/confd/conf.d/haproxy.toml
-    if [ "$?" != "0" ]; then
-        echo confd cannot generate initial configuration, retrying in 10 sec...
-        sleep 10
-    else
-        break
-    fi
-done
+    # attempt to generate a first configuration
+    while [ true ]; do
+        echo renerating config first time...
+        ./confd -onetime -backend zookeeper -node ${ZK} -config-file /etc/confd/conf.d/haproxy.toml
+        if [ "$?" != "0" ]; then
+            echo "watcher.sh: confd cannot generate initial configuration, retrying in 10 sec..."
+            sleep 5
+        else
+            break
+        fi
+    done
 
-# start the watch cycle
-while [ true ]; do
-    ./confd -interval 10 $ETCD_NODES -config-file /etc/confd/conf.d/haproxy.toml
-    echo confd exited, restarting.
-done
+    # start the watch cycle
+    while [ true ]; do
+        ./confd -interval 10 -backend zookeeper -node ${ZK} -config-file /etc/confd/conf.d/haproxy.toml
+        echo "watcher.sh: confd exited, restarting."
+    done
+
+else
+    echo "watcher.sh: is using etcd: ${ETCD_PEERS}"
+
+    # make sure the /services key is set in etcd before we start
+    for ETCD_URL in ${ETCD_PEERS//,/ }; do
+        curl -s -f $ETCD_URL/v2/keys/services -XPUT -d dir=true
+        if [ "$?" = "0" ]; then
+            break
+        fi
+    done
+
+    # attempt to generate a first configuration
+    ETCD_NODES="-node ${ETCD_PEERS//,/ -node }"
+    while [ true ]; do
+        ./confd -onetime $ETCD_NODES -config-file /etc/confd/conf.d/haproxy.toml
+        if [ "$?" != "0" ]; then
+            echo "watcher.sh: confd cannot generate initial configuration, retrying in 10 sec..."
+            sleep 10
+        else
+            break
+        fi
+    done
+fi
