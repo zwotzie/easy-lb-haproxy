b8c1e35f0b9a40c6897968889ef44c48c0ef86f7
Ralf Hense <ralf.hense@1und1.de>
2018-02-04 16:52:29 +0100

    get confd from release path

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+# build a docker image for this service
+
+pushd $(dirname ${BASH_SOURCE[0]})
+export SERVICE=$(basename $PWD)
+
+# confd releases: https://github.com/kelseyhightower/confd/releases
+[ -e confd ] && rm confd
+wget https://github.com/kelseyhightower/confd/releases/download/v0.15.0/confd-0.15.0-linux-amd64
+mv confd-0.15.0-linux-amd64 confd
+
 docker build -t zwotzie/easy-lb-haproxy .
+
+popd
